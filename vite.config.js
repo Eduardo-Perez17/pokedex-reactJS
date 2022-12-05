6b5a6ef853e15a137482ad05cbe63497cd569de5
Eduardo Perez <a.eduardoperez.fp2019@gmail.com>
2022-12-05 04:20:44 -0400

    configurando para produccion

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokedex-reactJS/',
+  base: 'https://Eduardo-Perez17.github.io/pokedex-reactJS/',
 });
